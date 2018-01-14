f5c582d1458a8da61cd357953b79dda50d797145
Baldapps <playappassistance@gmail.com>
2018-01-14 15:21:39 +0100

    CastManagerBuilder can now manage more than one BaseCastMaanger

--- src/com/google/android/libraries/cast/companionlibrary/cast/CastManagerBuilder.java
+++ src/com/google/android/libraries/cast/companionlibrary/cast/CastManagerBuilder.java
@@ -1,21 +1,44 @@
 package com.google.android.libraries.cast.companionlibrary.cast;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * You can add and manage multiple cast manager. A manager is added
+ * calling build. The order is important. The first cast manager is used
+ * as default inside the library. For example if you use the
+ * VideoCastNotificationService, the first manager must be an instance
+ * of VideoCastManager.
+ */
+@SuppressWarnings("unchecked")
 public class CastManagerBuilder {
 
     private static CastManagerFactory factory;
-    private static BaseCastManager castManager;
+    private static Map<String, BaseCastManager> castManagers = new LinkedHashMap<>();
 
     public static void setFactory(CastManagerFactory f) {
         factory = f;
     }
 
-    public static void build(Context context, CastConfiguration configuration) {
-        castManager = factory.build(context, configuration);
+    public static void build(Context context, String key, CastConfiguration configuration) {
+        castManagers.put(key, factory.build(context, configuration));
     }
 
-    public static <T extends BaseCastManager> T getCastManager() {
-        return (T) castManager;
+    @NonNull
+    public static <T extends BaseCastManager> T getCastManager() throws IllegalStateException {
+        if (castManagers.size() == 0)
+            throw new IllegalStateException("No cast managers configured");
+        Map.Entry<String, BaseCastManager> entry = castManagers.entrySet().iterator().next();
+        return (T) entry.getValue();
+    }
+
+    @NonNull
+    public static <T extends BaseCastManager> T getCastManager(String key) throws IllegalStateException {
+        if (castManagers.size() == 0)
+            throw new IllegalStateException("No cast managers configured");
+        return (T) castManagers.get(key);
     }
 }
