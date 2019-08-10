c0697027cabccc5b570e44729e4d808ce6d504fb
Baldapps <playappassistance@gmail.com>
2019-08-10 17:02:11 +0200

    Fix dependency

--- src/com/google/android/libraries/cast/companionlibrary/cast/ui/BaseCompatActivity.java
+++ src/com/google/android/libraries/cast/companionlibrary/cast/ui/BaseCompatActivity.java
@@ -1,9 +1,10 @@
 package com.google.android.libraries.cast.companionlibrary.cast.ui;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.libraries.cast.companionlibrary.cast.CastManagerBuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseCompatActivity extends AppCompatActivity {
     @Override
